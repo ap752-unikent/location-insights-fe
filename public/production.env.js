7c582fc4b37cd51b9c3039a56073900b68290984
Adam Preston <adampreston2010@hotmail.co.uk>
2025-01-04 23:44:21 +0000

    fix: change prod env var

--- public/production.env.js
+++ public/production.env.js
@@ -1,3 +1,3 @@
 window.env = {
-    API_URL: "https://backend-public-api-gohoushare-dev-f2d8hmatbsa2hte5.uksouth-01.azurewebsites.net",
+    API_URL: "https://backend-public-api-gohoushare-prod-fhdcase3bzehgac3.uksouth-01.azurewebsites.net",
 };
