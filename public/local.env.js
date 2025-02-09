156acb256fba4b71ecbbdb42776dc09fd63e1162
Adam Preston <adampreston2010@hotmail.co.uk>
2025-02-09 19:31:17 +0000

    fix: added a few more images

--- public/local.env.js
+++ public/local.env.js
@@ -1,4 +1,4 @@
 window.env = {
-    //API_URL: "https://backend-public-api-gohoushare-dev-f2d8hmatbsa2hte5.uksouth-01.azurewebsites.net",
-    API_URL: "http://localhost:8083",
+    API_URL: "https://backend-public-api-gohoushare-dev-f2d8hmatbsa2hte5.uksouth-01.azurewebsites.net",
+    //API_URL: "http://localhost:8083",
   };
