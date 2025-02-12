72d5e96f8af8ef3e3b7c613b2ac9626ebe184a97
Adam Preston <adampreston2010@hotmail.co.uk>
2025-02-12 15:53:57 +0000

    fix: use cdn

--- public/development.env.js
+++ public/development.env.js
@@ -1,3 +1,4 @@
 window.env = {
-    API_URL: "https://backend-public-api-gohoushare-dev-f2d8hmatbsa2hte5.uksouth-01.azurewebsites.net",
+    //API_URL: "https://backend-public-api-gohoushare-dev-f2d8hmatbsa2hte5.uksouth-01.azurewebsites.net",
+    API_URL: "https://backend-public-api-dev.azureedge.net"
 };
--- public/production.env.js
+++ public/production.env.js
@@ -1,3 +1,4 @@
 window.env = {
-    API_URL: "https://backend-public-api-gohoushare-prod-fhdcase3bzehgac3.uksouth-01.azurewebsites.net",
+    //API_URL: "https://backend-public-api-gohoushare-prod-fhdcase3bzehgac3.uksouth-01.azurewebsites.net",
+    API_URL: "https://backend-public-api-prod.azureedge.net"
 };
