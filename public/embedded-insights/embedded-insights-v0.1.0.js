cbeef2c091df72206a9efd3108cb49977500a381
Adam Preston <adampreston2010@hotmail.co.uk>
2025-03-04 15:07:07 +0000

    fix: update first version

--- public/embedded-insights/embedded-insights-v0.1.0.js
+++ public/embedded-insights/embedded-insights-v0.1.0.js
@@ -1,4 +1,12 @@
-const scriptTag = document.currentScript;
+import "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/shoelace.js";
+const { registerIconLibrary } = await import("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/utilities/icon-library.js");
+
+registerIconLibrary('lucide', {
+    resolver: name => `https://cdn.jsdelivr.net/npm/lucide-static@0.477.0/icons/${name}.svg`
+});
+
+const scriptTags = document.querySelectorAll("script");
+let scriptTag = Array.from(scriptTags).find(script => script.getAttribute("data-partner-property-id"));
 const partnerPropertyId = scriptTag.getAttribute("data-partner-property-id");
 const partnerId = scriptTag.getAttribute("data-partner-id");
 const baseUrl = scriptTag.getAttribute("data-base-url");
@@ -27,7 +35,12 @@ const fetchInsights = async () => {
         const emoji = value.emoji
         const text = value.text
 
-        innerHTML += `<li style='display:flex; align-items:center; gap:16px;'><span>${emoji}</span><span>${text}</span></li>`
+        innerHTML += `<li style='display:flex; align-items:center; gap:16px;'>
+                <div style="display:flex; color: #434667; font-size: 20px;">
+                    <sl-icon library="lucide" name="${emoji}"></sl-icon>
+                </div>
+                <span>${text}</span>
+            </li>`
     }
 
     innerHTML += "</ul>"
